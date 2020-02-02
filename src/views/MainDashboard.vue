0d5f61c0753a9d5bed8f31c395d933bfff1174db
Krystal Mitchell <10262399@my.uvu.edu>
2020-02-01 17:22:05 -0700

    test for netlify

--- src/views/MainDashboard.vue
+++ src/views/MainDashboard.vue
@@ -1,82 +1,65 @@
 <template>
   <v-container>
-    
-    <Header/>
-    <h1 style="font-size: 40px; margin-top: 40px">Test Who's Learning?</h1>
-      
-      <v-container>
-        <v-layout>
-          <v-flex xs12 sm6 style="display: flex;">
-            <v-card href="/studentdashboard" hover height="auto" class="studentCard">
-                <v-card-title 
-                    class="studentAvatar"
-                >
-                    <v-img
-                    class="avatarImage"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
+    <Header />
+    <h1 style="font-size: 40px; margin-top: 40px">Who's Learning?</h1>
 
-                    />
-                </v-card-title>
-                <h2 style="padding: 10px">Cameron</h2>    
-            </v-card>
-            <v-card href="/studentdashboard" hover height="auto" class="studentCard">
-                <v-card-title 
-                    class="studentAvatar"
-                >
-                    <v-img
-                    class="avatarImage"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
-
-                    />
-                </v-card-title>
-                <h2 style="padding: 10px">Sally</h2>    
-            </v-card>
-            <br>
-            <v-card href="/studentdashboard" hover height="auto" class="studentCard">
-                <v-card-title 
-                    class="studentAvatar"
-                >
-                    <v-img
-                    class="avatarImage"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
-
-                    />
-                </v-card-title>
-                <h2 style="padding: 10px">John</h2>    
-            </v-card>
-            <v-card href="#" hover height="auto" class="studentCard addStudent">
-                <v-card-title 
-                    class="studentAvatar"
-                >
-                    <v-img
-                    class="avatarImage"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
-
-                    />
-                </v-card-title>
-                <h2 style="padding: 10px">+ Add</h2>    
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-
-  
+    <v-container>
+      <v-layout>
+        <v-flex xs12 sm6 style="display: flex;">
+          <v-card href="/studentdashboard" hover height="auto" class="studentCard">
+            <v-card-title class="studentAvatar">
+              <v-img
+                class="avatarImage"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
+              />
+            </v-card-title>
+            <h2 style="padding: 10px">Cameron</h2>
+          </v-card>
+          <v-card href="/studentdashboard" hover height="auto" class="studentCard">
+            <v-card-title class="studentAvatar">
+              <v-img
+                class="avatarImage"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
+              />
+            </v-card-title>
+            <h2 style="padding: 10px">Sally</h2>
+          </v-card>
+          <br />
+          <v-card href="/studentdashboard" hover height="auto" class="studentCard">
+            <v-card-title class="studentAvatar">
+              <v-img
+                class="avatarImage"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
+              />
+            </v-card-title>
+            <h2 style="padding: 10px">John</h2>
+          </v-card>
+          <v-card href="#" hover height="auto" class="studentCard addStudent">
+            <v-card-title class="studentAvatar">
+              <v-img
+                class="avatarImage"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_RduMlXDb9A-mc_msw8SXpfyv5L3pZvZyLKFKYdf1ZTM4zdd&s"
+              />
+            </v-card-title>
+            <h2 style="padding: 10px">+ Add</h2>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 
 <script>
-import Header from '../components/Header' 
+import Header from "../components/Header";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header
   },
 
-  data: () => ({
-       
-  }),
-}
+  data: () => ({})
+};
 </script>
 
 <style lang="sass" scoped>
