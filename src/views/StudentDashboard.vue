dcf2722eb5328a55b2bed30152019fc61ae69a6a
Krystal Mitchell <10262399@my.uvu.edu>
2020-02-09 11:07:14 -0700

    maindashboard, student dashboard work

--- src/views/StudentDashboard.vue
+++ src/views/StudentDashboard.vue
@@ -1,68 +1,77 @@
 <template>
-  <div>
-    <Header/>
-    <v-header>  
-      <LogInModal/>
-    </v-header>
-    
-    <v-navigation-drawer v-model="drawer" app="app" stateless="stateless" floating="floating" width="220">
-      <v-list>
-          <v-img
-        class="logo"
-        src="/assets/images/FLC-Logo-Large-2b.png"
-        style="width: 100px"
-      />
-        <v-list-tile v-for="(title, icon) in mainMenu" @click="" style="display: flex">
-          
-          <v-list-tile-action>
-            <v-icon> {{ icon }}</v-icon>
-          </v-list-tile-action>
-      
-          <v-list-tile-content>
-            <v-list-tile-title> {{ title }}</v-list-tile-title>
-          </v-list-tile-content>  
+  <v-container class="page-container">
+    <Header />
 
+    <v-container style="display: flex;">
+      <!-- <v-navigation-drawer
+        v-model="drawer"
+        app="app"
+        stateless="stateless"
+        floating="floating"
+        width="220"
+        style="border: 2px solid red"
+      > -->
+      <v-list style="width: 40px;">
+        <v-list-tile
+          v-for="(menuItem, key) in sideMenu"
+          :key="key"
+          style="display: flex;"
+        >
+          <v-list-tile-action>
+            <v-icon> {{ menuItem.icon }}</v-icon>
+          </v-list-tile-action>
         </v-list-tile>
       </v-list>
+      <!-- </v-navigation-drawer> -->
+      <!-- </v-container> -->
 
-    </v-navigation-drawer>
-
-    <v-content> 
+      <v-content>
         <h1>Cameron</h1>
         <p>Here we can conditionally pull each page</p>
-    </v-content>
-  </div>
+      </v-content>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
-import CRM from '../components/CRM';
-import LogInModal from '../components/LogInModal';
-import Vue from 'vue';//check this since i moved everything around
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 export default {
-  name: 'App',
-  el: '#app',
+  name: "App",
+  el: "#app",
 
-  data: () => ({
-    drawer: true,
-    mainMenu: {
-      home: '',
-      person: '',
-      info: '',
-      settings: '',
-    },
-  }),
-
-  methods: {
-    clickToggleDrawer() {
-      this.drawer = !this.drawer
-    },
+  data() {
+    return {
+      // import student data for this component
+      sideMenu: [
+        {
+          icon: "home",
+          key: "home"
+        },
+        {
+          icon: "person",
+          key: "person"
+        },
+        {
+          icon: "info",
+          key: "info"
+        },
+        {
+          icon: "settings",
+          key: "settings"
+        }
+      ]
+    };
   },
-  
+
+  // methods: {
+  //   clickToggleDrawer() {
+  //     this.drawer = !this.drawer;
+  //   }
+  // },
+
   components: {
-    LogInModal,
     Header
-  },
+  }
 };
 </script>
