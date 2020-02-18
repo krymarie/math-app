93da697cfadbe71ee6af0bff07c0b93e0de73fa3
Krystal Mitchell <10262399@my.uvu.edu>
2020-02-18 14:27:36 -0700

    lessondashboard and progressive stuff

--- src/views/LessonDashboard.vue
+++ src/views/LessonDashboard.vue
@@ -40,36 +40,54 @@
           :width="30"
         ></v-progress-circular>
         <v-stepper v-model="e6" vertical>
-          <!-- itterate ofer units available from db with a v-for units as unit... to created these -->
           <v-stepper-step :complete="e6 > 1" step="1">
             Select a Unit
             <small>Unit 1</small>
           </v-stepper-step>
 
-          <!-- use @click="e6 = 2" at end of unit to progess to the next unit -->
           <v-stepper-content step="1">
-            <v-btn color="primary" href="/lessondashboard">Begin Unit 1</v-btn>
-            <v-btn text @click="e6 = 2">SKIP FOR DEV ONLY</v-btn>
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
+            <v-btn text>Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-step :complete="e6 > 2" step="2">Unit 2</v-stepper-step>
 
           <v-stepper-content step="2">
-            <v-btn color="primary" @click="e6 = 3">Begin Unit 2</v-btn>
-            <v-btn text>Back</v-btn>
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
+            <v-btn text>Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-step :complete="e6 > 3" step="3">Unit 3</v-stepper-step>
 
           <v-stepper-content step="3">
-            <v-btn color="primary" @click="e6 = 4">Begin Unit 3</v-btn>
-            <v-btn text>Back</v-btn>
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
+            <v-btn text>Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-step step="4">Unit 4</v-stepper-step>
           <v-stepper-content step="4">
-            <v-btn color="primary" @click="e6 = 1">Begin Unit 4</v-btn>
-            <v-btn text>Back</v-btn>
+            <v-card
+              color="grey lighten-1"
+              class="mb-12"
+              height="200px"
+            ></v-card>
+            <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
+            <v-btn text>Cancel</v-btn>
           </v-stepper-content>
         </v-stepper>
       </v-content>
